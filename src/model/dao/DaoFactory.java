438f4344ddffd2fe88f839d0bdfa1233ecc1c7a6
Edsontb1981 <edsontb1981@gmail.com>
2025-07-20 20:55:24 -0300

    findById implmentation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,15 @@
 package model.dao;
 
-public class DaoFactory {
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.SellerDao;
 
+
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
 }
